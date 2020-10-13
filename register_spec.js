af763867112909576b82130049827897ba7b2e86
Bijan Patel <bijan@binary.com>
2020-10-13 09:06:27 +0530

    First Commit

---
+++ register_spec.js
@@ -0,0 +1,48 @@
+describe("Join As Member", function(){
+
+    beforeAll(function(){
+        browser.get("https://www.istockphoto.com/join");
+
+        browser.driver.manage().window().maximize();
+    });
+
+    it("Enter company email", function(){
+
+        var txtEmail = element(by.model("register['email']"));
+
+        txtEmail.clear();
+
+        txtEmail.sendKeys("test1@demo.com");
+
+        expect(txtEmail.getAttribute("value")).toEqual("test1@demo.com");
+
+
+    });
+
+    it("Enter password", function(){
+
+        var txtPassword = element(by.model("register['password']"));
+
+        txtPassword.clear();
+
+        txtPassword.sendKeys("test@123");
+
+        expect(txtPassword.getAttribute("value")).toEqual("test@123");
+
+
+    });
+
+    it("Enter confirm password", function(){
+
+        var txtConfirmPassword = element(by.model("register['passwordConfirmation']"));
+
+        txtConfirmPassword.clear();
+
+        txtConfirmPassword.sendKeys("test@123");
+
+        expect(txtConfirmPassword.getAttribute("value")).toEqual("test@123");
+
+
+    });
+
+});
