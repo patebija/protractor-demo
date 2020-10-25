7b53153b0d01f4d90c2c6d41ec04a85fdf4f9486
Bijan Patel <bijan@binary.com>
2020-10-25 15:31:00 +0530

    Add config to generate HTML Reports

--- conf.js
+++ conf.js
@@ -1,4 +1,7 @@
 // An example configuration file.
+
+var Jasmine2HtmlReporter = require('C:/Users/Bijan/AppData/Roaming/npm/node_modules/protractor-jasmine2-html-reporter');
+
 exports.config = {
   directConnect: true,
 
@@ -12,10 +15,28 @@ exports.config = {
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['login_spec.js'],
+  specs: ['register_spec.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
-  }
+  },
+
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(
+      new Jasmine2HtmlReporter({
+        savePath: './reports/',
+        screenshotsFolder: 'images',
+        takeScreenshots: true,
+        takeScreenshotsOnlyOnFailures: true,
+        fileNameDateSuffix: true,
+        fileNamePrefix: 'Protractor',
+        fileName: 'HTMLReport',
+        fileNameSeparator: '_'
+      })
+    );
+ }
+
+
+
 };
